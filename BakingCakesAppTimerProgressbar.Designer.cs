65db2bdfc839e5a8a9aa3ec133a0f17271039d96
Elif Sülün <suluneliif@gmail.com>
2025-06-16 08:35:19 +0300

    BakingCakesApp
    Döngüler konusunda timer ve progressbar kullanarak kek yapım sürecini görselleştiren bir arayüz tasarladım. Renklendirme aşamasında if-else karar yapısından faydalandım.

---
+++ BakingCakesAppTimerProgressbar.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Loops
+{
+    partial class BakingCakesAppTimerProgressbar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BakingCakesAppTimerProgressbar));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.MistyRose;
+            this.progressBar1.ForeColor = System.Drawing.Color.Brown;
+            this.progressBar1.Location = new System.Drawing.Point(-1, 93);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(657, 45);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MistyRose;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(13, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yumurta ile şeker çırpılıyor...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MistyRose;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(13, 178);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sıvı malzemeler ekleniyor...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MistyRose;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Location = new System.Drawing.Point(13, 297);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Katı malzemeler ekleniyor...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MistyRose;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label4.Location = new System.Drawing.Point(13, 429);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fırında pişiriliyor...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.BackColor = System.Drawing.Color.MistyRose;
+            this.progressBar2.ForeColor = System.Drawing.Color.Brown;
+            this.progressBar2.Location = new System.Drawing.Point(-1, 207);
+            this.progressBar2.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(657, 45);
+            this.progressBar2.TabIndex = 8;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.BackColor = System.Drawing.Color.MistyRose;
+            this.progressBar3.ForeColor = System.Drawing.Color.Brown;
+            this.progressBar3.Location = new System.Drawing.Point(-1, 326);
+            this.progressBar3.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(657, 45);
+            this.progressBar3.TabIndex = 9;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.BackColor = System.Drawing.Color.MistyRose;
+            this.progressBar4.ForeColor = System.Drawing.Color.Brown;
+            this.progressBar4.Location = new System.Drawing.Point(-1, 458);
+            this.progressBar4.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(657, 45);
+            this.progressBar4.TabIndex = 10;
+            // 
+            // BakingCakesAppTimerProgressbar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(655, 585);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "BakingCakesAppTimerProgressbar";
+            this.Text = "BakingCakesAppTimerProgressbar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+    }
+}
---
+++ BakingCakesAppTimerProgressbar.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops
+{
+    public partial class BakingCakesAppTimerProgressbar : Form
+    {
+        public BakingCakesAppTimerProgressbar()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+
+            if (progressBar1.Value % 20 == 10)
+            {
+                label1.BackColor = Color.MistyRose;
+            }
+
+            if (progressBar1.Value % 20 == 0)
+            {
+                label1.BackColor = Color.HotPink;
+            }
+
+            if (progressBar1.Value == 100)
+            {
+                timer1.Stop();
+                timer2.Start();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            progressBar2.Value += 20;
+
+            if (progressBar2.Value % 40 == 0)
+            {
+                label2.BackColor = Color.MistyRose;
+            }
+            else
+            {
+                label2.BackColor = Color.HotPink;
+            }
+
+            if (progressBar2.Value == 100)
+            {
+                timer2.Stop();
+                timer3.Start();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            progressBar3.Value += 20;
+
+            if (progressBar3.Value % 40 == 0)
+            {
+                label3.BackColor = Color.MistyRose;
+            }
+            else
+            {
+                label3.BackColor = Color.HotPink;
+            }
+
+            if (progressBar3.Value == 100)
+            {
+                timer3.Stop();
+                timer4.Start();
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            progressBar4.Value += 5;
+
+            if (progressBar4.Value % 10 == 0)
+            {
+                label4.BackColor = Color.HotPink;
+            }
+            else
+            {
+                label4.BackColor = Color.MistyRose;
+            }
+
+            if (progressBar4.Value == 100)
+            {
+                timer4.Stop();
+                MessageBox.Show("Afiyet olsun :)");
+            }
+
+        }
+    }
+}
